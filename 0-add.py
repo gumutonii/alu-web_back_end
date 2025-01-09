ece408cc39aa1fb31705015b8858808bc325d262
gumutonii <g.umutoni@alustudent.com>
2025-01-09 14:27:13 +0200

    question 0

---
+++ 0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module 0-add
+This module contains a type-annotated function that performs addition of two floats.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers and returns their sum.
+
+    Args:
+        a (float): The first float number.
+        b (float): The second float number.
+
+    Returns:
+        float: The sum of the two float numbers.
+    """
+    return a + b
