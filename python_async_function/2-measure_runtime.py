873f7920ceb92c35c0c873b3975d8e1eb25cc770
gumutonii <g.umutoni@alustudent.com>
2025-01-10 18:51:36 +0200

    done

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-''' Measure the runtime '''
-import time
+''' Run time for four parallel comprehensions '''
 import asyncio
+import time
 
-wait_n = __import__('1-concurrent_coroutines').wait_n
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-def measure_time(n: int, max_delay: int) -> float:
-    ''' Function that returns the time '''
-    first = time.perf_counter()
-    asyncio.run(wait_n(n, max_delay))
-    finish = time.perf_counter()
-    total_time = finish - first
-    return total_time / n
+async def measure_runtime() -> float:
+    ''' Function to coutne '''
+    start = time.perf_counter()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    end = time.perf_counter()
+    return end - start
